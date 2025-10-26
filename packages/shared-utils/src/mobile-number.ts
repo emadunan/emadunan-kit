92a5dd4eedff04a16d3ba17ae391431ffd4dd7d1
emadunan <emadunan@gmail.com>
2025-10-26 15:59:42 +0300

    chore: Remove not used libphonenumber-js package and all functions depends on it

--- packages/shared-utils/src/mobile-number.ts
+++ packages/shared-utils/src/mobile-number.ts
@@ -1,14 +1,3 @@
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
-
-export function isValidMobile(number: string) {
-  try {
-    const phoneNumber = parsePhoneNumberFromString(number);
-    return phoneNumber?.isValid() ?? false;
-  } catch {
-    return false;
-  }
-}
-
 export function formatEgyptianMobile(number: string): string | false {
   try {
     if (!number) return false;
